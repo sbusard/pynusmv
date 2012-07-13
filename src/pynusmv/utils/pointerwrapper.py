b664915506aab2ae358f97edad4f7eaaf83c6d77
Simon Busard <simon.busard@uclouvain.be>
2012-07-13 16:23:21 +0200

    Add freeit to PointerWrapper.

--- src/pynusmv/utils/pointerwrapper.py
+++ src/pynusmv/utils/pointerwrapper.py
@@ -1,6 +1,11 @@
 class PointerWrapper:
     """Wrapper for a NuSMV pointer."""
     
-    def __init__(self, pointer):
-        """pointer -- the pointer to wrap."""
+    def __init__(self, pointer, freeit = False):
+        """
+        pointer -- the pointer to wrap.
+        freeit -- whether the pointer has to be freed when this wrapper
+                  is destroyed.
+        """
         self._ptr = pointer
+        self._freeit = freeit
