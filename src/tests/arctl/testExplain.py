58e7091b6517d3d7df8d497fda38b1b95f1781f4
Simon Busard <simon.busard@uclouvain.be>
2012-08-01 21:27:19 +0200

    Implement ARCTL explain EaU and EaG.

--- src/tests/arctl/testExplain.py
+++ src/tests/arctl/testExplain.py
@@ -5,7 +5,7 @@ from pynusmv.init.init import init_nusmv, deinit_nusmv
 
 from tools.arctl.parsing import parseArctl
 from tools.arctl.eval import evalArctl, evalArctl_from_string as evalStr
-from tools.arctl.explain import explain_eax
+from tools.arctl.explain import explain_eax, explain_eau, explain_eag
 
 
 class TestExplain(unittest.TestCase):
@@ -23,21 +23,88 @@ class TestExplain(unittest.TestCase):
         return fsm
         
     def test_eax_explain(self):
-       fsm = self.init_model()
-       self.assertIsNotNone(fsm)
+        fsm = self.init_model()
+        self.assertIsNotNone(fsm)
+        
+        spec = parseArctl("E<'TRUE'>X 'c1.c = 1'")[0]
+        specbdd = evalArctl(fsm, spec)
+        
+        s = fsm.pick_one_state(specbdd & fsm.init)
+        self.assertIsNotNone(s)
+        
+        ac = evalStr(fsm, "'TRUE'")
+        phi = evalStr(fsm, "'c1.c = 1'")
+        
+        path = explain_eax(fsm, s, ac, phi)
+        
+        self.assertTrue(s == path[0])
+        self.assertTrue(path[1] <= ac)
+        self.assertTrue(path[2] <= phi)
+        self.assertTrue(path[2] <= fsm.post(s, path[1]))
        
-       spec = parseArctl("E<'TRUE'>X 'c1.c = 1'")[0]
-       specbdd = evalArctl(fsm, spec)
        
-       s = fsm.pick_one_state(specbdd & fsm.init)
-       self.assertIsNotNone(s)
-       
-       ac = evalStr(fsm, "'TRUE'")
-       phi = evalStr(fsm, "'c1.c = 1'")
-       
-       path = explain_eax(fsm, s, ac, phi)
-       
-       self.assertTrue(s == path[0])
-       self.assertTrue(path[1] <= ac)
-       self.assertTrue(path[2] <= phi)
-       self.assertTrue(path[2] <= fsm.post(s, path[1]))
+    def test_eau_explain(self):
+        fsm = self.init_model()
+        self.assertIsNotNone(fsm)
+        
+        spec = parseArctl("E<'TRUE'>['c1.c < 2' U 'c1.c = 2']")[0]
+        specbdd = evalArctl(fsm, spec)
+        
+        s = fsm.pick_one_state(specbdd & fsm.init)
+        self.assertTrue((specbdd & fsm.init).isnot_false())
+        self.assertIsNotNone(s)
+        self.assertTrue(s.isnot_false())
+        
+        ac = evalStr(fsm, "'TRUE'")
+        phi = evalStr(fsm, "'c1.c < 2'")
+        psi = evalStr(fsm, "'c1.c = 2'")
+        
+        path = explain_eau(fsm, s, ac, phi, psi)
+        
+        # Check that path contains states of phi and psi
+        self.assertTrue(s == path[0])
+        for state, inputs in zip(path[::2], path[1::2]):
+            self.assertTrue(state <= phi)
+            self.assertTrue(inputs <= ac)
+        self.assertTrue(path[-1] <= psi)
+        
+        # Check that path is a path of fsm
+        for s, i, sp in zip(path[::2], path[1::2], path[2::2]):
+            self.assertTrue(sp <= fsm.post(s, i))
+            
+            
+    def test_eag_explain(self):
+        fsm = self.init_model()
+        self.assertIsNotNone(fsm)
+        
+        spec = parseArctl("E<'run = rc1'>G 'c1.c <= 2'")[0]
+        specbdd = evalArctl(fsm, spec)
+        
+        s = fsm.pick_one_state(specbdd & fsm.init)
+        self.assertTrue((specbdd & fsm.init).isnot_false())
+        self.assertIsNotNone(s)
+        self.assertTrue(s.isnot_false())
+        
+        ac = evalStr(fsm, "'run = rc1'")
+        phi = evalStr(fsm, "'c1.c <= 2'")
+        
+        (path, (inputs, loop)) = explain_eag(fsm, s, ac, phi)
+        
+        # loop and inputs are not None: the explanation is an infinite path
+        self.assertIsNotNone(inputs)
+        self.assertIsNotNone(loop)
+        
+        # Check that path contains states of phi and psi
+        self.assertTrue(s == path[0])
+        for state, inputs in zip(path[::2], path[1::2]):
+            self.assertTrue(state <= phi)
+            self.assertTrue(inputs <= ac)
+        self.assertTrue(path[-1] <= phi)
+        
+        # Check that path is a path of fsm
+        for s, i, sp in zip(path[::2], path[1::2], path[2::2]):
+            self.assertTrue(sp <= fsm.post(s, i))
+            
+        # Check that loop is effectively a loop
+        self.assertTrue(loop in path)
+        self.assertTrue(loop <= fsm.post(path[-1], inputs))
--- src/tools/arctl/explain.py
+++ src/tools/arctl/explain.py
@@ -2,6 +2,9 @@
 ARCTL explain functions.
 """
 
+from pynusmv.dd.bdd import BDD
+from .eval import _ex, eag, _eu, eau, eax
+
 def explain_eax(fsm, state, alpha, phi):
     """
     Explain why state of fsm satisfies E<a>X p.
@@ -37,7 +40,14 @@ def explain_eag(fsm, state, alpha, phi):
     alpha -- a BDD of fsm representing the inputs satisfying a;
     phi -- a BDD of fsm representing the states satisfying p.
     
-    Return a tuple (path, (inputs, loop)) where path is a tuple (s0, i1, .., sn)
+    Return a tuple (path, (inputs, loop)).
+    
+    If inputs and loop are None, this means that the witness is a full finite
+    path and that path[-1] satisfies ~E<'alpha'>X 'TRUE' (dead state). In this
+    case, path is a tuple (s0, i1, ..., sn) where sj |= phi for all j,
+    ij |= alpha for all j and sj -ij+1-> sj+1 for all j.
+    
+    Otherwise, path is a tuple (s0, i1, ..., sn)
     where
         s0 = state
         sj -ij+1-> sj+1 for all j : 0 <= j < n
@@ -47,7 +57,44 @@ def explain_eag(fsm, state, alpha, phi):
         ij belongs to alpha for all j : 0 <= j <= n
         inputs belongs to alpha.
     """
-    pass # TODO
+    # eag(a, p) = _eu(a, p, p & ~_ex(a, true)) | _eg(a, p)
+    
+    paext = phi & ~_ex(fsm, alpha, BDD.true(fsm.bddEnc.DDmanager))
+    euppaext = _eu(fsm, alpha, phi, paext)
+    
+    # If state satisfies _eu(a, p, p & ~_ex(a, true)),
+    # use explain_eau to extract such a path
+    if state <= euppaext:
+        return (explain_eau(fsm, alpha, phi, paext), (None, None))
+    
+    # Otherwise,
+    else:    
+        # Get allstates = E<alpha>G phi
+        allstates = eag(fsm, alpha, phi)
+    
+        # Start path at s
+        path = [state]
+        # While path[-1] cannot reach itself through states of eag,
+        while (path[-1] &
+               eax(fsm,
+                   alpha,
+                   eau(fsm, alpha, allstates, path[-1]))).is_false():
+            # choose a successor of path[-1] in eag and add it in path
+            path.append(fsm.post(state, alpha) & allstates)
+    
+        # At this point, path[-1] can reach itself through states of eag
+        # Explain it with explain_eau and explain_eax
+        eaus = eau(fsm, alpha, allstates, path[-1])
+        first = explain_eax(fsm, path[-1], alpha, eaus)
+        second = explain_eau(fsm, first[-1], alpha, allstates, path[-1])
+        fs = first + second[1:]
+    
+        # Store the loop
+        inputs = fs[-2]
+        loop = fs[-1]
+    
+        # Return the path and the loop
+        return (fs[:-2], (inputs, loop))
     
     
 def explain_eau(fsm, state, alpha, phi, psi):
@@ -67,4 +114,34 @@ def explain_eau(fsm, state, alpha, phi, psi):
         sn belongs to psi
         ij belongs to alpha for all j : 0 <= j <= n.
     """
-    pass # TODO
+    
+    # Compute fixpoint and store intermediate BDDs
+    funct = lambda Z: (psi | (phi & _ex(fsm, alpha, Z)))
+    old = BDD.false(fsm.bddEnc.DDmanager)
+    new = funct(old)
+    paths = [new]
+    # Stop when reaching state
+    # This is ensured since state satisfies E<a>[p U q]
+    while (state & new).is_false():
+        old = new
+        new = funct(old)
+        paths.append(new - old)
+        
+    # paths contains intermediate BDDs
+    # paths[i] contains the BDD of all states of phi
+    # that can reach a state of psi
+    # through states of phi, in i steps
+    # restricted to paths of alpha actions
+    
+    # paths[-1] contains state, skip it
+    paths = paths[:-1]
+    s = state
+    path = [s]
+    for states in paths[::-1]:
+        sp = fsm.pick_one_state(fsm.post(s, alpha) & states)
+        i = fsm.pick_one_inputs(fsm.get_inputs_between_states(s, sp))
+        path.append(i)
+        path.append(sp)
+        s = sp
+    
+    return tuple(path)
