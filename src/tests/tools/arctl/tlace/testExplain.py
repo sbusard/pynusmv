e976b81f95726c4f87af94d78b30ef2684887c18
Simon Busard <simon.busard@uclouvain.be>
2012-08-03 16:39:28 +0200

    Tests on ARCTL TLACE generation and XML representation. Basic tests.

---
+++ src/tests/tools/arctl/tlace/testExplain.py
@@ -0,0 +1,50 @@
+import unittest
+
+from pynusmv.fsm.fsm import BddFsm
+from pynusmv.init.init import init_nusmv, deinit_nusmv
+
+from tools.arctl.parsing import parseArctl
+from tools.arctl.check import checkArctl
+from tools.arctl.tlace.explain import explain_witness, explain_countex
+
+
+class TestExplain(unittest.TestCase):
+    
+    def setUp(self):
+        init_nusmv()
+    
+    def tearDown(self):
+        deinit_nusmv()
+    
+    
+    def init_model(self):
+        fsm = BddFsm.from_filename("tests/tools/arctl/counters.smv")
+        self.assertIsNotNone(fsm)
+        return fsm
+        
+    
+    def init_finite_model(self):
+        fsm = BddFsm.from_filename("tests/tools/arctl/finite_counters.smv")
+        self.assertIsNotNone(fsm)
+        return fsm
+        
+
+    def test_full(self):
+        fsm = self.init_model()
+        
+        spec = parseArctl("A<'run = rc1'>["
+                            "E<'TRUE'>G 'c1.c < 3' W "
+                            "A<'TRUE'>X 'c1.c = 0' ]")[0]
+                            
+        (result, expl) = checkArctl(fsm, spec, explain_witness, explain_countex)
+        self.assertTrue(result)
+        
+        
+        
+    def test_full_finite(self):
+        fsm = self.init_finite_model()
+        
+        spec = parseArctl("A<'TRUE'>F (~ E<'TRUE'>X 'TRUE')")[0]
+        
+        (result, expl) = checkArctl(fsm, spec, explain_witness, explain_countex)
+        self.assertTrue(result)
---
+++ src/tests/tools/arctl/tlace/testXML.py
@@ -0,0 +1,58 @@
+import unittest
+
+from pynusmv.fsm.fsm import BddFsm
+from pynusmv.init.init import init_nusmv, deinit_nusmv
+
+from tools.arctl.parsing import parseArctl
+from tools.arctl.check import checkArctl
+from tools.arctl.tlace.explain import explain_witness, explain_countex
+
+from tools.arctl.tlace.xml import xml_representation
+
+
+class TestXML(unittest.TestCase):
+    
+    def setUp(self):
+        init_nusmv()
+    
+    def tearDown(self):
+        deinit_nusmv()
+    
+    
+    def init_model(self):
+        fsm = BddFsm.from_filename("tests/tools/arctl/counters.smv")
+        self.assertIsNotNone(fsm)
+        return fsm
+        
+    
+    def init_finite_model(self):
+        fsm = BddFsm.from_filename("tests/tools/arctl/finite_counters.smv")
+        self.assertIsNotNone(fsm)
+        return fsm
+        
+
+    def test_full(self):
+        fsm = self.init_model()
+        
+        spec = parseArctl("A<'run = rc1'>["
+                            "E<'TRUE'>G 'c1.c < 3' W "
+                            "A<'TRUE'>X 'c1.c = 0' ]")[0]
+                            
+        (result, expl) = checkArctl(fsm, spec, explain_witness, explain_countex)
+        self.assertTrue(result)
+        
+        self.assertIsNotNone(xml_representation(fsm, expl, spec))
+        print(xml_representation(fsm, expl, spec))
+        
+        
+        
+    def test_full_finite(self):
+        fsm = self.init_finite_model()
+        
+        spec = parseArctl("A<'TRUE'>F (~ E<'TRUE'>X 'TRUE')")[0]
+        
+        (result, expl) = checkArctl(fsm, spec, explain_witness, explain_countex)
+        self.assertTrue(result)
+        
+        self.assertIsNotNone(xml_representation(fsm, expl, spec))
+        print(xml_representation(fsm, expl, spec))
