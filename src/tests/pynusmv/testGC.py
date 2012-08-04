2ef5bc93205704013b817e03877ea6960e500779
Simon Busard <simon.busard@uclouvain.be>
2012-08-04 21:54:45 +0200

    Add doc in testGC.py

--- src/tests/pynusmv/testGC.py
+++ src/tests/pynusmv/testGC.py
@@ -6,6 +6,12 @@ from pynusmv.fsm.fsm import BddFsm
 class TestGC(unittest.TestCase):
     
     def test_gc(self):
+        """
+        This test should not produce a segfault due to freed memory after
+        deiniting NuSMV. This is thanks to the PyNuSMV GC system that keeps
+        track of all wrapped pointers and free them when deiniting NuSMV, if
+        needed.
+        """
         init_nusmv()
         
         fsm = BddFsm.from_filename("tests/pynusmv/admin.smv")
