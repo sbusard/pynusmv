d73e153606ae40dd89e20341ecdfa2374cec5209
Simon Busard <simon.busard@uclouvain.be>
2012-09-21 22:08:35 +0200

    Add check TLACE module.

---
+++ src/tools/ctlk/tlace/check.py
@@ -0,0 +1,23 @@
+from ..eval import evalCTLK
+from .explain import explain_witness, explain_countex
+
+def checkCTLK(fsm, spec):
+    """
+    Check whether fsm satisfies spec.
+    
+    fsm -- a MAS
+    spec -- a CTLK specification
+    
+    Return a tuple (sat, diag) where
+        sat is True iff fsm satisfies spec, it is False otherwise;
+        diag is a TLACE explaining why fsm satisfies or violates spec.
+    """
+    
+    sat = evalCTLK(fsm, spec)
+    violating = fsm.init & ~sat
+    if violating.isnot_false():
+        return (False,
+                explain_countex(fsm, fsm.pick_one_state(violating), spec))
+    else:
+        return (True,
+                explain_witness(fsm, fsm.pick_one_state(fsm.init), spec))
