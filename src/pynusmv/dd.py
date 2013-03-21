d35e8c62e4dc91f7cdce209376b94abb0901056d
Simon Busard <simon.busard@uclouvain.be>
2013-03-21 21:52:29 +0100

    Add StateInputs class for state/inputs pairs.

--- src/pynusmv/dd.py
+++ src/pynusmv/dd.py
@@ -708,3 +708,56 @@ class State(BDD):
         
         """
         return State(nsdd.bdd_dup(bdd._ptr), fsm)
+
+
+class StateInputs(BDD):
+    """
+    Python class for State and Inputs structure.
+    
+    A StateInputs is a :class:`BDD` representing a single state/inputs pair
+    of the model.
+    
+    """
+    
+    def __init__(self, ptr, fsm, freeit = True):
+        super().__init__(ptr, fsm.bddEnc.DDmanager, freeit)
+        self._fsm = fsm
+
+    
+    def get_str_values(self):
+        """
+        Return a dictionary of the (variable, value) pairs of this StateInputs.
+        
+        :rtype: a dictionary of pairs of strings.
+        
+        """
+        enc = self._fsm.bddEnc
+        # Get symb table from enc (BaseEnc)
+        table = enc.symbTable
+
+        # Get symbols (SymbTable) for states
+        layers = nssymb_table.SymbTable_get_class_layer_names(table._ptr, None)
+        symbols = nssymb_table.SymbTable_get_layers_sf_symbols(table._ptr, layers)
+        isymbols = nssymb_table.SymbTable_get_layers_i_symbols(table._ptr, layers)
+        nsutils.NodeList_concat(symbols, isymbols)
+        nsutils.NodeList_destroy(isymbols)
+        
+        # Get assign symbols (BddEnc)
+        assignList = nsbddEnc.BddEnc_assign_symbols(enc._ptr,self._ptr,
+                                                  symbols, 0, None)
+
+        values = {}
+        # Traverse the symbols to print variables of the state
+        asList_ptr = assignList
+        while asList_ptr:
+            assignment = nsnode.car(asList_ptr)
+            var = nsnode.car(assignment)
+            val = nsnode.cdr(assignment)
+            values[nsnode.sprint_node(var)] = nsnode.sprint_node(val)
+            asList_ptr = nsnode.cdr(asList_ptr)
+            
+        nsnode.free_list(assignList)
+        
+        nsutils.NodeList_destroy(symbols)
+            
+        return values
