c8ce42cf94222a7a4cec7e32c0cd5634073e11f7
Simon Busard <simon.busard@uclouvain.be>
2013-03-29 11:17:33 +0100

    First tests on implementing split for ATLK^F_po.

---
+++ src/tests/tools/atlkPO/testSplit.py
@@ -0,0 +1,151 @@
+import unittest
+
+from pynusmv.dd import BDD
+from pynusmv.init import init_nusmv, deinit_nusmv
+from pynusmv.mc import eval_simple_expression
+
+from pynusmv.nusmv.dd import dd as nsdd
+
+from tools.mas import glob
+
+
+class TestSplit(unittest.TestCase):
+    
+    def setUp(self):
+        init_nusmv()
+    
+    def tearDown(self):
+        glob.reset_globals()
+        deinit_nusmv()
+    
+    
+    def little(self):
+        glob.load_from_file("tests/tools/atlkPO/little.smv")
+        fsm = glob.mas()
+        self.assertIsNotNone(fsm)
+        return fsm
+        
+    def show_si(self, fsm, bdd):
+        for si in fsm.pick_all_states_inputs(bdd):
+            print(si.get_str_values())
+            
+    def show_s(self, fsm, bdd):
+        for s in fsm.pick_all_states(bdd):
+            print(s.get_str_values())
+            
+    def show_i(self, fsm, bdd):
+        for i in fsm.pick_all_inputs(bdd):
+            print(i.get_str_values())
+        
+    def test_split_by_picking(self):
+        fsm = self.little()
+        
+        aa = eval_simple_expression(fsm, "a.a = 1")
+        ap = eval_simple_expression(fsm, "a.p = 1")
+        ba = eval_simple_expression(fsm, "b.a = 1")
+        bq = eval_simple_expression(fsm, "b.q = 1")
+        true = eval_simple_expression(fsm, "TRUE")
+        false = eval_simple_expression(fsm, "FALSE")
+        
+        excluded = BDD.false(fsm.bddEnc.DDmanager)
+        
+        strats = fsm.protocol({"a"})
+        print("protocol of a -- strats to consider")
+        self.show_si(fsm, strats)
+        si = fsm.pick_one_state_inputs(strats)
+        self.assertEqual(len(fsm.pick_all_states(
+                                            si.forsome(fsm.bddEnc.inputsCube))),
+                         1)
+        s = fsm.pick_one_state(si.forsome(fsm.bddEnc.inputsCube))
+        print("si:", si.get_str_values())
+        print("s:", s.get_str_values())
+        eqs = fsm.equivalent_states(s, {"a"})
+        print("equivalent to", s.get_str_values())
+        self.show_s(fsm, eqs)
+        eqcl = strats & eqs
+        print("equivalence class (" + str(s.get_str_values()) + ")")
+        self.show_si(fsm, eqcl)
+        act = eqcl.forsome(fsm.bddEnc.statesCube)
+        print("actions of eq class (" + str(fsm.count_inputs(act)) + ")")
+        self.show_i(fsm, act)
+        
+        
+        gamma_inputs = [agent+"."+var
+                         for agent in {"a"}
+                         for var in fsm.agents_inputvars[agent]]
+        gamma_cube = fsm.bddEnc.cube_for_inputs_vars(gamma_inputs)
+        ngamma_ptr = nsdd.bdd_cube_diff(fsm.bddEnc.DDmanager._ptr,
+                                        fsm.bddEnc.inputsCube._ptr,
+                                        gamma_cube._ptr)
+        ngamma_cube = BDD(ngamma_ptr, fsm.bddEnc.DDmanager, freeit=True)
+        
+        
+        nact = ~act.forsome(ngamma_cube) # Useless here
+        print("the other actions")
+        self.show_i(fsm, nact)
+        
+        # There is more than one action if act contains other actions than
+        # si
+        self.assertTrue((act -
+                         si.forsome(fsm.bddEnc.statesCube).
+                            forsome(ngamma_cube)).is_false())
+        print("Are there other actions than si's one ?",
+              (act - si.forsome(fsm.bddEnc.statesCube).
+                                  forsome(ngamma_cube)).isnot_false())
+        
+        # remove act from Strats
+        excluded = excluded | (strats & eqcl)
+        print("excluded")
+        self.show_si(fsm, excluded)
+        strats = strats - excluded
+        print("new strats")
+        self.show_si(fsm, strats)
+        
+        # Restart the process
+        si = fsm.pick_one_state_inputs(strats)
+        self.assertEqual(len(fsm.pick_all_states(
+                                            si.forsome(fsm.bddEnc.inputsCube))),
+                         1)
+        s = fsm.pick_one_state(si.forsome(fsm.bddEnc.inputsCube))
+        print("si:", si.get_str_values())
+        print("s:", s.get_str_values())
+        eqs = fsm.equivalent_states(s, {"a"})
+        print("equivalent to", s.get_str_values())
+        self.show_s(fsm, eqs)
+        eqcl = strats & eqs
+        print("equivalence class (" + str(s.get_str_values()) + ")")
+        self.show_si(fsm, eqcl)
+        act = eqcl.forsome(fsm.bddEnc.statesCube)
+        print("actions of eq class (" + str(fsm.count_inputs(act)) + ")")
+        self.show_i(fsm, act)
+        
+        print("Are there other actions than si's one ?",
+              (act - si.forsome(fsm.bddEnc.statesCube).
+                                  forsome(ngamma_cube)).isnot_false())
+        self.show_i(fsm, (act - si.forsome(fsm.bddEnc.statesCube).
+                                   forsome(ngamma_cube)))
+                                   
+        # Need to split eqcl now !
+        # Keep eqcl to split strats later
+        fulleqcl = eqcl
+        ncss = (eqcl & si.forsome(fsm.bddEnc.statesCube).
+                          forsome(ngamma_cube))
+        eqcls = [ncss]
+        eqcl = eqcl - ncss
+        while eqcl.isnot_false():
+            si = fsm.pick_one_state_inputs(eqcl)
+            ncss = (eqcl & si.forsome(fsm.bddEnc.statesCube).
+                              forsome(ngamma_cube))
+            eqcls.append(ncss)
+            eqcl = eqcl - ncss
+        for ncss in eqcls:
+            print("new non-conflicting subset")
+            self.show_si(fsm, ncss)
+            
+        splitted = []
+        for ncss in eqcls:
+            splitted.append(strats - fulleqcl + ncss + excluded)
+        print("show splitted strats")
+        for substrat in splitted:
+            print("new sub strategy")
+            self.show_si(fsm, substrat)
