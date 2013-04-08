f9a1aee5739041d6bc0b06b73ada7e791d8a24ad
Simon Busard <simon.busard@uclouvain.be>
2013-04-08 16:11:19 +0200

    Add test to show the problem with picking all inputs.

---
+++ src/tests/pynusmv/testPick.py
@@ -0,0 +1,56 @@
+import unittest
+
+from pynusmv.dd import BDD, StateInputs
+from pynusmv.init import init_nusmv, deinit_nusmv
+from pynusmv import glob
+from pynusmv.mc import eval_simple_expression
+
+from pynusmv.nusmv.dd import dd as nsdd
+
+
+class TestEval(unittest.TestCase):
+    
+    def setUp(self):
+        init_nusmv()
+    
+    def tearDown(self):
+        deinit_nusmv()
+    
+    
+    def model(self):
+        glob.load_from_file("tests/pynusmv/models/inputs.smv")
+        glob.compute_model()
+        fsm = glob.prop_database().master.bddFsm
+        self.assertIsNotNone(fsm)
+        return fsm
+        
+    def show_si(self, fsm, bdd):
+        for si in fsm.pick_all_states_inputs(bdd):
+            print(si.get_str_values())
+            
+    def show_s(self, fsm, bdd):
+        for s in fsm.pick_all_states(bdd):
+            print(s.get_str_values())
+            
+    def show_i(self, fsm, bdd):
+        for i in fsm.pick_all_inputs(bdd):
+            print(i.get_str_values())
+
+
+    def test_pick(self):
+        fsm = self.model()
+        
+        p = eval_simple_expression(fsm, "p")
+        a = eval_simple_expression(fsm, "a")
+        b0 = eval_simple_expression(fsm, "b = 0")
+        b1 = eval_simple_expression(fsm, "b = 1")
+        b2 = eval_simple_expression(fsm, "b = 2")
+        true = eval_simple_expression(fsm, "TRUE")
+        false = eval_simple_expression(fsm, "FALSE")
+        
+        for i in fsm.pick_all_inputs(a & b1):
+            si = StateInputs(i._ptr, i._fsm, i._freeit)
+            print(si.get_str_values(), si._ptr)
+            print(i.get_str_values(), i._ptr)
+            
+        self.assertEqual(len(fsm.pick_all_inputs(a & b1)), 1)
