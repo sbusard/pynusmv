ed69853b390a3651668974a54da620ccde3e7ccd
Simon Busard <simon.busard@uclouvain.be>
2013-05-11 08:22:20 +0200

    Update PyNuSMV version in setup.py.

--- src/setup.py
+++ src/setup.py
@@ -1115,7 +1115,7 @@ extensions.append(
 
 
 setup(  name = "PyNuSMV",
-        version = "0.7",
+        version = "0.8",
         author = "Simon Busard",
         author_email = "simon.busard@uclouvain.be",
         url = "http://lvl.info.ucl.ac.be/Tools/PyNuSMV",
