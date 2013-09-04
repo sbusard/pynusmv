92636dc77c8098afb34a38716af62a5b870d4555
Simon Busard <simon.busard@uclouvain.be>
2013-09-04 10:21:09 +0200

    Add CLI to ATL explanation.

--- src/tools/atl/explain.py
+++ src/tools/atl/explain.py
@@ -2,7 +2,6 @@ from pynusmv.dd import BDD
 
 from .eval import ceg, ceu, cew
 from tools.explanation.explanation import Explanation
-    
 
 def explain_cex(fsm, state, agents, phi):
     """
@@ -363,3 +362,220 @@ def explain_caw(fsm, state, agents, phi, psi):
         nodes[cur].successors.add((act, nodes[succ]))
     
     return nodes[state]
+    
+    
+if __name__ == '__main__':
+    import argparse, sys
+    from pynusmv.init import init_nusmv, deinit_nusmv
+    from ..mas import glob
+    from .parsing import parseATL
+    from .eval import evalATL
+    from .check import check
+    from pyparsing import ParseException
+    from pynusmv.exception import PyNuSMVError
+    from .ast import CEF, CEG, CEX, CEU, CEW, CAF, CAG, CAX, CAU, CAW
+    
+    def explain_top(mas, spec):
+        """
+        Explain why the top operator of spec is satisfied.
+        
+        mas -- the concerned MAS;
+        spec -- the given specification; must be satisfied by mas.
+        """
+        
+        # Get satisfying initial state
+        sat = evalATL(mas, spec)
+        state = mas.pick_one_state(
+                                 (sat & mas.bddEnc.statesInputsMask & mas.init))
+                                
+        # Switch between operators
+        if type(spec) is CEF:
+            phi = BDD.true(mas.bddEnc.DDmanager)
+            psi = evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_ceu(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CEG:
+            phi = evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_ceg(mas, state, agents, phi)
+            
+        elif type(spec) is CEX:
+            phi = evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cex(mas, state, agents, phi)
+            
+        elif type(spec) is CEU:
+            phi = evalATL(mas, spec.left)
+            psi = evalATL(mas, spec.right)
+            agents = {atom.value for atom in spec.group}
+            return explain_ceu(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CEW:
+            phi = evalATL(mas, spec.left)
+            psi = evalATL(mas, spec.right)
+            agents = {atom.value for atom in spec.group}
+            return explain_cew(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CAF:
+            phi = BDD.true(mas.bddEnc.DDmanager)
+            psi = evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cau(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CAG:
+            phi = evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cag(mas, state, agents, phi)
+            
+        elif type(spec) is CAX:
+            phi = evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cax(mas, state, agents, phi)
+            
+        elif type(spec) is CAU:
+            phi = evalATL(mas, spec.left)
+            psi = evalATL(mas, spec.right)
+            agents = {atom.value for atom in spec.group}
+            return explain_cau(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CAW:
+            phi = evalATL(mas, spec.left)
+            psi = evalATL(mas, spec.right)
+            agents = {atom.value for atom in spec.group}
+            return explain_caw(mas, state, agents, phi, psi)
+            
+        else:
+            return Explanation(state)
+            
+        
+    def cntex_top(mas, spec):
+        """
+        Explain why the top operator of spec is violated.
+        
+        mas -- the concerned MAS;
+        spec -- the given specification; must be violated by mas.
+        """
+        
+        # Get violating initial state
+        sat = evalATL(mas, spec)
+        state = mas.pick_one_state(
+                                (~sat & mas.bddEnc.statesInputsMask & mas.init))
+                                
+        # Switch between operators
+        if type(spec) is CEF:
+            # ~<a> F p = [a] G ~p
+            phi = ~evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cag(mas, state, agents, phi)
+            
+        elif type(spec) is CEG:
+            # ~<a> G p = [a] F ~p = [a] true U ~p
+            phi = BDD.true(mas.bddEnc.DDmanager)
+            psi = ~evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cau(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CEX:
+            # ~<a> X p = [a] X ~p
+            phi = ~evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cax(mas, state, agents, phi)
+            
+        elif type(spec) is CEU:
+            # ~<a> p U q = [q] ~q W ~p & ~q
+            p = evalATL(mas, spec.left)
+            q = evalATL(mas, spec.right)
+            phi = ~q
+            psi = ~p & ~q
+            agents = {atom.value for atom in spec.group}
+            return explain_caw(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CEW:
+            # ~<a> p W q = [q] ~q U ~p & ~q
+            p = evalATL(mas, spec.left)
+            q = evalATL(mas, spec.right)
+            phi = ~q
+            psi = ~p & ~q
+            agents = {atom.value for atom in spec.group}
+            return explain_cau(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CAF:
+            # ~[a] F p = <a> G ~p
+            phi = ~evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_ceg(mas, state, agents, phi)
+            
+        elif type(spec) is CAG:
+            # ~[a] G p = <a> F ~p = <a> true U ~p
+            phi = BDD.true(mas.bddEnc.DDmanager)
+            psi = ~evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_ceu(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CAX:
+            # ~[a] X p = <a> X ~p
+            phi = ~evalATL(mas, spec.child)
+            agents = {atom.value for atom in spec.group}
+            return explain_cex(mas, state, agents, phi)
+            
+        elif type(spec) is CAU:
+            # ~[a] p U q = <q> ~q W ~p & ~q
+            p = evalATL(mas, spec.left)
+            q = evalATL(mas, spec.right)
+            phi = ~q
+            psi = ~p & ~q
+            agents = {atom.value for atom in spec.group}
+            return explain_cew(mas, state, agents, phi, psi)
+            
+        elif type(spec) is CAW:
+            # ~[a] p W q = <q> ~q U ~p & ~q
+            p = evalATL(mas, spec.left)
+            q = evalATL(mas, spec.right)
+            phi = ~q
+            psi = ~p & ~q
+            agents = {atom.value for atom in spec.group}
+            return explain_ceu(mas, state, agents, phi, psi)
+            
+        else:
+            return Explanation(state)
+    
+    
+    # Parse arguments
+    description = """ATL model checking explainer.
+    
+    The truth value of the given property is printed on stderr,
+    a dot-formated explanation is printed on stdout.
+    A counter-example is printed if the property is violated,
+    a witness is printed otherwise.
+    Only the top operator is explained.
+    """
+    parser = argparse.ArgumentParser(description=description)
+    # Populate arguments: the model, the property
+    parser.add_argument('model', help='the MAS as an SMV model')
+    parser.add_argument('property', help='the property to explain')
+    args = parser.parse_args()
+    
+    init_nusmv()
+    # Initialize the model
+    glob.load_from_file(args.model)
+    mas = glob.mas()
+    
+    try:
+        # Parse property
+        spec = parseATL(args.property)[0]
+        
+        # Check property
+        satisfied = check(mas, spec)
+        
+        print('Specification', str(spec), 'is', str(satisfied), file=sys.stderr)
+        
+        if satisfied:
+            print(explain_top(mas, spec).dot())
+        else:
+            print(cntex_top(mas, spec).dot())
+    except ParseException as e:
+        print("[ERROR] Cannot parse specification:", str(e))
+    except PyNuSMVError as e:
+        print("[ERROR]", str(e))
+    
--- src/tools/fairctl/explain.py
+++ src/tools/fairctl/explain.py
@@ -1,7 +1,39 @@
 from pynusmv.dd import BDD
 
-from .eval import ex, eg, ceu
+from .eval import ex, eg, eu, fair_states
 from tools.explanation.explanation import Explanation
+
+
+def explain_one_succ(fsm, state, sat):
+    """
+    Explain why state can reach sat in one step.
+    The returned value is a tuple (action, successor)
+    where successor is a successor of state through action belonging to sat.
+    
+    fsm -- the fsm;
+    state -- a state of fsm able to reach sat in one step;
+    sat -- a BDD representing a set of states of fsm.    
+    """
+    next = fsm.pick_one_state(fsm.post(state) & sat)
+    inputs = fsm.pick_one_inputs(fsm.get_inputs_between_states(state, next))
+    return (inputs, next)
+    
+
+def explain_all_succ(fsm, state, sat):
+    """
+    Give all the successors of state belonging to sat.
+    The returned value is a set of tuples (action, successor)
+    where successor is a successor of state through action belonging to sat.
+    
+    fsm -- the fsm;
+    state -- a state of fsm able to reach sat in one step;
+    sat -- a BDD representing a set of states of fsm.    
+    """
+    successors = set()
+    for next in fsm.pick_all_states(fsm.post(state) & sat)
+        inputs = fsm.pick_one_inputs(fsm.get_inputs_between_states(state, next))
+        successors.add((inputs, next))
+    return successors
     
 
 def explain_ex(fsm, state, phi):
@@ -13,22 +45,63 @@ def explain_ex(fsm, state, phi):
     phi -- the set of states of fsm satifying phi.
     """
     # To show that state satisfies EX phi, we have to exhibit a successor
-    # of state belonging to phi.
-    # We also have to show that the reached state is fair.
-    pass # TODO
+    # of state belonging to phi that is reachable and fair.
+    
+    # EX phi = fsm.pre(phi & fair)
+    
+    # We don't explain why the extracted successor is fair.
+    
+    phi = phi & fair_states(fsm) & fsm.reachable_states
+    
+    # Get successor of state belonging to phi
+    inputs, next = explain_one_succ(fsm, state, phi)
+    
+    return Explanation(state, {(inputs, Explanation(next))})
     
     
-def explain_eu(fsm, state, phi, psi, states=None):
+def explain_eu(fsm, state, phi, psi):
     """
     Explain why state of fsm satisfies E phi U psi.
     
     fsm -- the fsm;
     state -- a state of fsm satisfying E phi U psi;
     phi -- the set of states of fsm satifying phi;
-    psi -- the set of states of fsm satifying psi;
-    states -- a dictionary of state->explanation pairs.
+    psi -- the set of states of fsm satifying psi.
     """
-    pass # TODO
+    # E phi U psi = mu Z . (psi & fair) | (phi & fsm.pre(Z))
+    
+    # To show that state satisfies E phi U psi
+    # we have to show a path of successors satisfying phi,
+    # ending in a fair state satisfying psi.
+    
+    # If state satisfies psi, stop;
+    # otherwise, compute the fixpoint until state is in the last set,
+    # then show that state can reach the previous set,
+    # and recursively explain why the reached state satisfies E phi U psi.
+    
+    # We don't explain why the psi-states are fair.
+    
+    if state <= psi:
+        return Explanation(state)
+        
+    else:
+        # Compute the fixpoint until state is reached (backward)
+        fair = fair_states(fsm)
+        f = lambda Y : (psi & fair) | (phi & fsm.pre(Y))
+        old = BDD.false(fsm.bddEnc.DDmanager)
+        new = psi
+        while not state <= new:
+            old = new
+            new = f(old)
+            
+        # If state <= new, (and it was not the case in the previous iteration)
+        # old does not contain state but a successor of state.
+        
+        # explain that state can reach old
+        inputs, next = explain_one_succ(fsm, state, old)
+        successors = {(inputs, explain_eu(fsm, next, phi, psi))}
+        
+        return Explanation(state, successors)
     
     
 def explain_eg(fsm, state, phi):
@@ -39,7 +112,45 @@ def explain_eg(fsm, state, phi):
     state -- a state of fsm satisfying EG phi;
     phi -- the set of states of fsm satifying phi.
     """
-    pass # TODO
+    # To explain why state satisfies EG phi,
+    # we have to show a path ending with a loop going through all fairness
+    # constraints
+    
+    # If no fairness constraints are provided,
+    # model checking sums up to
+    # EG p = nu Z. p & Pre(Z)
+    # In this case, just explain by extending EX EG p until a former state is
+    # encountered
+    if len(fsm.fairness_contraints) <= 0:
+        states = BDD.false(fsm.bddEnc.DDmanager)
+        orig_expl = expl = Explanation(state)
+        eg_phi = eg(fsm, phi)
+        while not state <= states:
+            states = states | state
+            if (fsm.post(state) & states).isnot_false():
+                # There is a successor of state that we already saw.
+                # Use this one
+                action, state = explain_one_succ(fsm, state, states)
+            else:
+                # There is no successor of state in states, guess a successor
+                action, state = explain_one_succ(fsm, state, eg_phi)
+            nextexpl = Explanation(state)
+            expl.successors = {(action, nextexpl)}
+            expl = nextexpl
+        # state <= states
+        # this means that state lead to a previous state in the path
+        # we must explain it
+        cur = orig_expl
+        while cur.state != state:
+            cur = cur.successors[0][1]
+        action = fsm.pick_one_inputs(
+                                fsm.get_inputs_between_states(state, cur.state))
+        expl.successors = {(action, cur)}
+        return orig_expl
+    
+    # Otherwise, there are fairness constraints
+    else:
+        pass # TODO
     
     
 def explain_ew(fsm, state, phi, psi):
@@ -51,7 +162,10 @@ def explain_ew(fsm, state, phi, psi):
     phi -- the set of states of fsm satifying phi;
     psi -- the set of states of fsm satifying psi.
     """
-    pass # TODO
+    if state <= eg(fsm, phi):
+        return explain_eg(fsm, state, phi)
+    else: # state <= eu(fsm, phi, psi)
+        return explain_eu(fsm, state, phi, psi)
     
     
 def explain_ax(fsm, state, phi):
@@ -64,7 +178,24 @@ def explain_ax(fsm, state, phi):
     """
     # To show that state satisfies AX phi, we have to show that all successors
     # satisfy phi or are not fair.
-    pass # TODO
+    
+    # AX phi = ~fsm.pre((~phi & fair))
+    
+    # We don't explain why the extracted successors are not fair (when it is
+    # the case).
+    
+    # phi or not fair state
+    phi = (phi | ~fair_states(fsm)) & fsm.reachable_states
+    
+    # Get successor of state belonging to phi
+    all_succ = explain_all_succ(fsm, state, phi)
+    
+    # Build successors
+    successors = set()
+    for inputs, next in all_succ:
+        succ.add((inputs, Explanation(next)))
+    
+    return Explanation(state, succ)
     
     
 def explain_au(fsm, state, phi, psi, states=None):
@@ -110,7 +241,7 @@ def explain_fair(fsm, state):
     fsm -- the fsm;
     state -- a fair state of fsm.
     """
-    pass # TODO
+    return explain_eg(fsm, state, BDD.true(fsm.bddEnc.DDmanager))
     
     
 def explain_not_fair(fsm, state):
