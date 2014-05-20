43594fba0a6ae88d7ec02b539ec53e659408a809
Simon Busard <simon.busard@uclouvain.be>
2014-05-20 13:40:04 +0200

    Add arguments for implementation and partial implem config in ATLK_PO checker.

--- src/tools/atlkPO/check.py
+++ src/tools/atlkPO/check.py
@@ -11,6 +11,8 @@ from .evalPartial import evalATLK as evalATLK_partial
 from pyparsing import ParseException
 from pynusmv.exception import PyNuSMVError
 
+from . import config
+
 __implementations = {"naive" : evalATLK_naive,
                      "generator" : evalATLK_gen,
                      "optimized" : evalATLK_opt,
@@ -67,19 +69,65 @@ def process(allargs):
     parser.add_argument('model', help='the MAS as an SMV model')
     parser.add_argument('-p', dest='property', help='the property check',
                         default=None)
-    parser.add_argument('-v', dest='variant', help='the variant to use',
+    parser.add_argument('-v', dest='variant',
+                        help='the variant to use (SF, FS, FSF)',
                         default="SF")
+    parser.add_argument('-i', dest='implementation',
+                        help='the implementation to use (' +
+                             ', '.join(__implementations.keys()) + ')',
+                        default="naive")
+    
+    # Debug
+    parser.add_argument('-d', dest='debug', help='activate debugging info display',
+                        action='store_true', default=False)
+
+    # Config-related arguments
+    parser.add_argument('-pe', dest='early',
+                        help='activate early termination for partial '
+                             'strategies: full, partial or threshold (int) '
+                             '(default: None)',
+                        default=None)
+    parser.add_argument('-pc', dest='caching', help='activate caching',
+                        action='store_true', default=False)
+    parser.add_argument('-pf', dest='filtering', help='activate filtering',
+                        action='store_true', default=False)
+    parser.add_argument('-ps', dest='separation',
+                        help='activate separation of states for partial '
+                             'strategies: random, reach (default: None)',
+                        default=None)
+    parser.add_argument('-g', dest='garbage',
+                        help='activate explicit garbage collection: '
+                        'each or step (int) (default: None)', default=None)
+    
+    
     args = parser.parse_args(allargs)
     
     # Initialize the model
     glob.load_from_file(args.model)
     mas = glob.mas()
     
+    # Configure model checking
+    config.debug = args.debug
+    try:
+        config.garbage.step = int(args.garbage)
+        config.garbage.type = "step"
+    except:
+        config.garbage.type = args.garbage
+    try:
+        config.partial.early.threshold = float(args.early)
+        config.partial.early.type = "threshold"
+    except:
+        config.partial.early.type = args.early
+    config.partial.caching = args.caching
+    config.partial.filtering = args.filtering
+    config.partial.separation.type = args.separation
+    
     # Check given property, if any
     if args.property:
         try:
             spec = parseATLK(args.property)[0]
-            satisfied = check(mas, spec, variant=args.variant)
+            satisfied = check(mas, spec, variant=args.variant,
+                              implem=args.implementation)
             print('Specification', str(spec), 'is', str(satisfied))
         except ParseException as e:
             print("[ERROR] Cannot parse specification:", str(e))
@@ -93,7 +141,8 @@ def process(allargs):
             try:
                 spec = parseATLK(line)[0]
             
-                satisfied = check(mas, spec)
+                satisfied = check(mas, spec, variant=args.variant,
+                                  implem=args.implementation)
                 print('Specification', str(spec), 'is', str(satisfied))
             except ParseException as e:
                 print("[ERROR] Cannot parse specification:", str(e))
